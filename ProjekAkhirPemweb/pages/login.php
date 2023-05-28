f8d29180714dfc67f6c0965ffe93f6a423b87934
hasfi <hasfirasya02@gmail.com>
2023-05-28 21:49:24 +0700

    connect database beta

---
+++ ProjekAkhirPemweb/pages/login.php
@@ -0,0 +1,37 @@
+<?php
+$host = 'localhost';
+$database = 'projectakhir';
+$username = 'root';
+$password = 'hasfirasya';
+
+// Create a connection
+$connection = new mysqli($host, $username, $password, $database);
+
+// Check if the connection was successful
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+// Retrieve the submitted username/email and password from the form
+$usernameEmail = $_POST['username'];
+$password = $_POST['password'];
+
+// Perform any necessary validation or sanitization on the input data
+
+// Query the database to check if the credentials are valid
+$sql = "SELECT * FROM project WHERE (username = '$usernameEmail' OR email = '$usernameEmail') AND password = '$password'";
+$result = $connection->query($sql);
+
+// Check if any rows were returned
+if ($result->num_rows > 0) {
+    // Valid credentials, user is authenticated
+    // Perform any necessary actions (e.g., set session variables, redirect to a dashboard page, etc.)
+    echo "Login successful!";
+} else {
+    // Invalid credentials, show an error message or redirect to a login failure page
+    echo "Invalid username/email or password!";
+}
+
+// Close the database connection
+$connection->close();
+?>
---
+++ ProjekAkhirPemweb/pages/signup.php
@@ -0,0 +1,67 @@
+<?php
+class DatabaseConnection {
+    private $host = 'localhost';
+    private $database = 'projectakhir';
+    private $username = 'root';
+    private $password = 'hasfirasya'; // Replace with your actual database password
+
+    private $connection;
+
+    public function __construct() {
+        // Create a connection
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        // Check if the connection was successful
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
+    }
+
+    public function signup($email, $password, $username) {
+        // Prepare the SQL statement
+        $stmt = $this->connection->prepare("INSERT INTO project (email, password, username) VALUES (?, ?, ?)");
+
+        // Bind the parameters
+        $stmt->bind_param("sss", $email, $password, $username);
+
+        // Execute the statement
+        if ($stmt->execute()) {
+            echo "Signup successful!";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+        header("Location: index.html");
+        exit();
+    }
+
+    public function closeConnection() {
+        // Close the connection
+        $this->connection->close();
+    }
+}
+
+// Create an instance of the DatabaseConnection class
+$db = new DatabaseConnection();
+
+// Check if the signup form was submitted
+if (isset($_POST['signup-button'])) {
+    // Get the form data
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $username = $_POST['username'];
+
+    // Call the signup method to insert the data into the database
+    $db->signup($email, $password, $username);
+}
+
+if ($connection->query($sql) === TRUE) {
+    // Sign-up successful, redirect to main menu (home.html) using JavaScript
+    echo '<script>window.location.href = "home.html";</script>';
+} else {
+    // Error occurred, display an error message or redirect to a failure page
+    echo "Error: " . $sql . "<br>" . $connection->error;
+}
+
+// Close the database connection
+$db->closeConnection();
+?>
